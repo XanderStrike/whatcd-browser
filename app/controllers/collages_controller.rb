a812ca5219810b334c9289f54f066fa6f6345f24
Alexander Standke <xanderstrike@gmail.com>
2017-10-28 14:50:16 -0700

    add sorting to collage index

--- app/controllers/collages_controller.rb
+++ app/controllers/collages_controller.rb
@@ -1,6 +1,7 @@
 class CollagesController < ApplicationController
   def index
-    @collages = Collage.where("Name like ? and TagList like ?", "%#{params[:search]}%", "%#{params[:tag]}%").paginate(page: params[:page], per_page: 30)
+    sort = params[:sort] || 'Name desc'
+    @collages = Collage.where("Name like ? and TagList like ?", "%#{params[:search]}%", "%#{params[:tag]}%").order(sort).paginate(page: params[:page], per_page: 30)
   end
 
   def show
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def sortable_header(title, column_name = title)
+    sorted_by_this = params[:sort]&.include?(column_name)
+    new_sort = if params[:sort] == "#{column_name} desc"
+                 "#{column_name} asc"
+               else
+                 "#{column_name} desc"
+               end
+
+    sort_direction = if params[:sort]&.include?('asc')
+                       '&uarr;'
+                     else
+                       '&darr;'
+                     end
+
+    link_to "#{title}&nbsp;#{sorted_by_this ? sort_direction : nil }".html_safe, params.permit!.merge(sort: new_sort)
+  end
 end
