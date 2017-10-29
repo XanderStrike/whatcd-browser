ce5b6e374780c09c2ea20caa2912f608abab64f8
Alexander Standke <xanderstrike@gmail.com>
2017-10-28 17:15:52 -0700

    add sorting for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,14 +1,12 @@
 class ArtistsController < ApplicationController
   def index
-    if params[:search].present?
-      @artists = ArtistGroup.where("Name like ?", "%#{params[:search]}%").paginate(page: params[:page], per_page: 30)
-    else
-      @artists = ArtistGroup.paginate(page: params[:page], per_page: 50)
-    end
+    sort = params[:sort] || 'Name asc'
+    @artists = ArtistGroup.where("Name like ?", "%#{params[:search]}%").order(sort).paginate(page: params[:page], per_page: 30)
   end
 
   def show
     @artist = ArtistGroup.find(params[:id])
-    @torrent_groups = @artist.torrent_groups.paginate(page: params[:page], per_page: 50)
+    sort = params[:sort] || 'Name asc'
+    @torrent_groups = @artist.torrent_groups.order(sort).paginate(page: params[:page], per_page: 50)
   end
 end
--- app/controllers/collages_controller.rb
+++ app/controllers/collages_controller.rb
@@ -1,6 +1,6 @@
 class CollagesController < ApplicationController
   def index
-    sort = params[:sort] || 'Name desc'
+    sort = params[:sort] || 'Name asc'
     @collages = Collage.where("Name like ? and TagList like ?", "%#{params[:search]}%", "%#{params[:tag]}%").order(sort).paginate(page: params[:page], per_page: 30)
   end
 
