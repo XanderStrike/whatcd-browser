950a900c7a0969edad2dbdcfa74861261481fcbd
Alexander Standke <xanderstrike@gmail.com>
2017-10-28 18:25:04 -0700

    add artist images and bios from lastfm

--- app/models/artist_group.rb
+++ app/models/artist_group.rb
@@ -10,4 +10,8 @@ class ArtistGroup < ActiveRecord::Base
   self.table_name = 'artists_group'
 
   has_many :torrent_groups, foreign_key: 'ArtistID'
+
+  def similar
+    ActiveRecord::Base.connection.execute("select sa.Name, s.SimilarID from artists_group a join artists_similar s on s.ArtistID = a.ArtistID join artists_group sa on sa.ArtistID = s.SimilarID  where a.ArtistID = #{self.ArtistID}").to_a
+  end
 end
